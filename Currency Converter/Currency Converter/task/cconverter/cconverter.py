90168ad272aac857dd9636f3514c77c177e01e1b
Jegors Čemisovs <yegor.chemisov@gmail.com>
2021-02-28 10:29:43 +0200

    Solved stage 2

--- Currency Converter/Currency Converter/task/cconverter/cconverter.py
+++ Currency Converter/Currency Converter/task/cconverter/cconverter.py
@@ -1,2 +1,8 @@
 # write your code here!
-print("Meet a conicoin!")
+conicoins = int(input())
+dollars = 100 * conicoins
+
+print(f"""I have {conicoins} conicoins.
+{conicoins} conicoins cost {dollars} dollars.
+I am rich! Yippee!""")
+
