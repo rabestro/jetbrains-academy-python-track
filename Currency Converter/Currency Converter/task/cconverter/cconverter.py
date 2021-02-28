a5b9931d6e381c9a9250fd675d1f9da59e0a560f
Jegors Čemisovs <yegor.chemisov@gmail.com>
2021-02-28 13:16:21 +0200

    Solved stage 3

--- Currency Converter/Currency Converter/task/cconverter/cconverter.py
+++ Currency Converter/Currency Converter/task/cconverter/cconverter.py
@@ -1,8 +1,7 @@
 # write your code here!
-conicoins = int(input())
-dollars = 100 * conicoins
+conicoins = int(input("Please, enter the number of conicoins you have:"))
+rate = float(input("Please, enter the exchange rate:"))
+dollars = rate * conicoins
+print(f"The total amount of dollars: {dollars}")
 
-print(f"""I have {conicoins} conicoins.
-{conicoins} conicoins cost {dollars} dollars.
-I am rich! Yippee!""")
 
