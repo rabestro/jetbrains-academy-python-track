c8064532ae04f285b4c6689dc226e27cb039011f
Jegors Čemisovs <yegor.chemisov@gmail.com>
2021-02-28 14:10:51 +0200

    Code refactored

--- Currency Converter/Currency Converter/task/cconverter/cconverter.py
+++ Currency Converter/Currency Converter/task/cconverter/cconverter.py
@@ -2,5 +2,5 @@
 rates = {"RUB": 2.98, "ARS": 0.82, "HNL": 0.17, "AUD": 1.9622, "MAD": 0.208}
 conicoins = float(input())
 
-for x in rates:
-    print(f"I will get {rates[x] * conicoins:.2f} {x} from the sale of {conicoins} conicoins.")
+for code, rate in rates.items():
+    print(f"I will get {rate * conicoins:.2f} {code} from the sale of {conicoins} conicoins.")
