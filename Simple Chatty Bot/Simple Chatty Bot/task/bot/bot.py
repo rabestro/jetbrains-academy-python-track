c93e82405915a8af8e87a2a2d01f3a2ca4ecfc8a
Jegors Čemisovs <yegor.chemisov@gmail.com>
2021-03-03 18:14:19 +0200

    Solved Stage 3

--- Simple Chatty Bot/Simple Chatty Bot/task/bot/bot.py
+++ Simple Chatty Bot/Simple Chatty Bot/task/bot/bot.py
@@ -1,7 +1,17 @@
 print('Hello! My name is Aid.')
 print('I was created in 2020.')
-print('Please, remind me your name.')
 
-# reading a name
-name = input()
-print(f'What a great name you have, {name}!')
+name = input('Please, remind me your name.')
+
+print(f'''
+What a great name you have, {name}!
+Let me guess your age.
+Enter remainders of dividing your age by 3, 5 and 7.''')
+
+# reading all remainders
+
+remainder3 = int(input())
+remainder5 = int(input())
+remainder7 = int(input())
+your_age = (remainder3 * 70 + remainder5 * 21 + remainder7 * 15) % 105
+print(f"Your age is {your_age}; that's a good time to start programming!")
