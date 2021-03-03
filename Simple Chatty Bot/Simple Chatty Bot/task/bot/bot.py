e0952ed35d3426cb7edd08dcc574f32cd9206378
Jegors Čemisovs <yegor.chemisov@gmail.com>
2021-03-03 18:40:13 +0200

    Solved Stage 5

--- Simple Chatty Bot/Simple Chatty Bot/task/bot/bot.py
+++ Simple Chatty Bot/Simple Chatty Bot/task/bot/bot.py
@@ -1,26 +1,57 @@
-print('Hello! My name is Aid.')
-print('I was created in 2020.')
+def greet(bot_name, birth_year):
+    print(f'''
+    Hello! My name is {bot_name}.
+    I was created in {birth_year}.
+    ''')
 
-name = input('Please, remind me your name.')
 
-print(f'''
-What a great name you have, {name}!
-Let me guess your age.
-Enter remainders of dividing your age by 3, 5 and 7.''')
+def remind_name():
+    name = input('Please, remind me your name.')
+    print(f'What a great name you have, {name}!')
 
-rem3 = int(input())
-rem5 = int(input())
-rem7 = int(input())
 
-age = (rem3 * 70 + rem5 * 21 + rem7 * 15) % 105
+def guess_age():
+    print('Let me guess your age.')
+    print('Enter remainders of dividing your age by 3, 5 and 7.')
 
-print(f"""Your age is {age}; that's a good time to start programming!
-Now I will prove to you that I can count to any number you want.""")
+    rem3 = int(input())
+    rem5 = int(input())
+    rem7 = int(input())
+    age = (rem3 * 70 + rem5 * 21 + rem7 * 15) % 105
 
-# read a number and count to it here
-number = int(input())
-counter = 0
-while counter <= number:
-    print(str(counter) + " !")
-    counter += 1
-print('Completed, have a nice day!')
+    print("Your age is " + str(age) + "; that's a good time to start programming!")
+
+
+def count():
+    print('Now I will prove to you that I can count to any number you want.')
+
+    num = int(input())
+    curr = 0
+    while curr <= num:
+        print(curr, '!')
+        curr = curr + 1
+
+
+def test():
+    print("Let's test your programming knowledge.")
+    while True:
+        answer = int(input('''Why do we use methods?
+        1. To repeat a statement multiple times.
+        2. To decompose a program into several small subroutines.
+        3. To determine the execution time of a program.
+        4. To interrupt the execution of a program.'''))
+        if answer == 2:
+            break
+    print('Completed, have a nice day!')
+
+
+def end():
+    print('Congratulations, have a nice day!')
+
+
+greet('Aid', '2020')  # change it as you need
+remind_name()
+guess_age()
+count()
+test()
+end()
