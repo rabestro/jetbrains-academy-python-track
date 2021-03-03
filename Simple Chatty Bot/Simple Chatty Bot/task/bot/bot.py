8e0843df6bf47f405be7e9b079cae6a888a8f35b
Jegors Čemisovs <yegor.chemisov@gmail.com>
2021-03-03 18:09:21 +0200

    Solved Stage 2

--- Simple Chatty Bot/Simple Chatty Bot/task/bot/bot.py
+++ Simple Chatty Bot/Simple Chatty Bot/task/bot/bot.py
@@ -1,3 +1,7 @@
-# write your code here
-print("""Hello! My name is Aid.
-I was created in 2020.""")
+print('Hello! My name is Aid.')
+print('I was created in 2020.')
+print('Please, remind me your name.')
+
+# reading a name
+name = input()
+print(f'What a great name you have, {name}!')
